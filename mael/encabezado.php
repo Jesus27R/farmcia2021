29c14a57764fd5256d9272aeb3c9b90d3e684a94
Jesus27R <100612110+Jesus27R@users.noreply.github.com>
2022-05-25 09:23:22 -0500

    Add files via upload

---
+++ mael/encabezado.php
@@ -0,0 +1,35 @@
+<?php
+//proyecto tesina para farmacias mael tapilula
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>FARMCIA MAEL</title>
+	<title>Ventas</title>
+	
+	<link rel="stylesheet" href="./css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="./css/2.css">
+	<link rel="stylesheet" href="./css/estilo.css">
+
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">POS</a>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="./listar.php">Productos</a></li>
+					<li><a href="./vender.php">Vender</a></li>
+					<li><a href="./ventas.php">Ventas</a></li>
+				</ul>
+			</div>
+		</div>
+		<body>
+  			<p style="color:hsl(100,800%,50%);">Farmacia Mael&copy; 2022 - Página creada por Jesus Rodriguez  - Todos los derechos reservados</p>
+		</body>
+	</nav>
+	<div class="container">
+		<div class="row">
